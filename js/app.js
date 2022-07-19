57ec7d6cc278bfd55eae5b72015d785b8c711514
Humberto Pineda <bpineda7540@gmail.com>
2022-07-18 23:19:35 -0700

    added hours array

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 'use strict';
 
+let storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+console.log(storeHours);
+
 let seattle = {
   name: 'Seattle',
   min: 23,
@@ -17,8 +20,10 @@ let seattle = {
     console.log(custPerHour);
   },
   totalCookPerStore: function() {
-    for (let i = 0; i < 14; i++)
-      console.log(this.totalCookPerStore);
+    for (let i = 0; i < storeHours.length; i++) {
+      
+      console.log(seattle.totalCookPerStore);
+    }
   }
 };
 console.log(seattle.randomCust());
